57b7d02ec20ce9ec5fa1dabe3048780173874518
JaceKramer <112589616+JaceKramer@users.noreply.github.com>
2024-03-02 21:50:25 -0600

    Delete comments

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,6 @@ import java.awt.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /*JFrame testFrame = new JFrame();
-        testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel displayPanel = new JPanel();*/
-        //displayPanel.add(new TeamPanel());
-        //displayPanel.add(new ShapeIconPanel());
-        /*displayPanel.add(new KeyBoardPanel());
-        displayPanel.add(new MousePanel());
-
-        testFrame.getContentPane().add(displayPanel);
-        testFrame.pack();
-        testFrame.setVisible(true);*/
 
         JFrame frame = new JFrame("Space Game");
         GamePanel gamePanel = new GamePanel();
